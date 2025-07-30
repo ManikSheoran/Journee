1603fbf2a56282a9736810eee83c31a787337313
MANIK <136956963+ManikSheoran@users.noreply.github.com>
2025-07-30 11:42:15 +0530

    env fixes

--- server/app.js
+++ server/app.js
@@ -57,8 +57,9 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      secure: true,
-      maxAge: 30 * 24 * 60 * 60 * 1000, 
+      secure: false,
+      sameSite: 'lax',
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     },
   })
 );

ca2eaac6d3091ded3950d86df09a85ad7bc6034d
MANIK <136956963+ManikSheoran@users.noreply.github.com>
2025-07-30 12:26:46 +0530

    Update auth handling and session cookie settings

--- server/app.js
+++ server/app.js
@@ -57,8 +57,8 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     },
   })
